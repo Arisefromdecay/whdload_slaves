9a9c81433779a71e6c647f67a634087691227fee
Arisefromdecay <stefan_fischer@gmx.de>
2026-04-20 21:34:55 +0200

    Add trainer and ctro

--- U/Ugh/usr/source/UghHD.s
+++ U/Ugh/usr/source/UghHD.s
@@ -1,351 +1,547 @@
-;*---------------------------------------------------------------------------
-;  :Program.	BoppinHD.asm
-;  :Contents.	Slave for "Boppin"
-;  :Author.	JOTD, from Wepl sources
-;  :Original	v1 
-;  :Version.	$Id: BoppinHD.asm 1.2 2002/02/08 01:18:39 wepl Exp wepl $
-;  :History.	%DATE% started
-;  :Requires.	-
-;  :Copyright.	Public Domain
-;  :Language.	68000 Assembler
-;  :Translator.	Devpac 3.14, Barfly 2.9
-;  :To Do.
-;---------------------------------------------------------------------------*
+;----------------------------------------------------------------------------- 
+; Author:       asman, JOTD
+; Version       1.3
+; History	    1.2 by asman and JOTD
+;				1.3 07.04.2026 Trainer added (and optional cracktro)
+; Requires	-
+; Copyright	Public Domain
+; Language	68000 Assembler
+; Translator	Barfly V2.9
+;
+; Game		Ugh
+;----------------------------------------------------------------------------- 
 
-	INCDIR	Include:
+	INCDIR	Includes:
 	INCLUDE	whdload.i
 	INCLUDE	whdmacros.i
-	INCLUDE	lvo/dos.i
+	INCLUDE	libraries/lowlevel.i
+;-----------------------------------------------------------------------------
 
-;CHIP_ONLY
+RELEASE	;uncomment, if you want make release version of slave'a
+IGNORE_JOY_DIRECTIONS
+;-----------------------------------------------------------------------------
+
 	IFD BARFLY
-	OUTPUT	"Ugh.slave"
-	IFND	DEBUG
-	BOPT	O+				;enable optimizing
-	BOPT	OG+				;enable optimizing
-	ENDC
-	BOPT	ODd-				;disable mul optimizing
-	BOPT	ODe-				;disable mul optimizing
-	BOPT	w4-				;disable 64k warnings
-	BOPT	wo-			;disable optimizer warnings
-	SUPER
-	ENDC
 
-;============================================================================
+	IFD RELEASE
 
+	OUTPUT	"HD2:util/dev/whdload/ugh/Ugh.slave"
 
-	IFD	CHIP_ONLY
-HRTMON
-CHIPMEMSIZE	= $100000
-FASTMEMSIZE	= $0000
+	BOPT	O+             ;enable optimizing
+	BOPT	OG+            ;enable optimizing
+
 	ELSE
-BLACKSCREEN
+
+	OUTPUT	"u.slave"
+	BOPT	O-             ;disable optimizing
+	BOPT	OG-            ;disable optimizing
+
+	ENDC	;END IFD RELEASE
+
+	BOPT   ODd-           ;disable mul optimizing
+	BOPT   ODe-           ;disable mul optimizing
+	BOPT   w4-            ;disable 64k warnings
+	BOPT   wo-            ;disable optimize warnings
+	SUPER
+
+	ENDC	;END IFD BARFLY
+
+;-----------------------------------------------------------------------------
+;; macro: used to WHDLoad slaves
+;
+; \1 - name of routine without prefix ( resload_ )
+;
+WHDL	MACRO
+		move.l	_resload(pc),a2
+		jsr	resload_\1(a2)
+	ENDM
+
+;-----------------------------------------------------------------------------
+
+Emulate_Key	MACRO
+
+;\1 JPB bit from lowlevel.i ( for example JPB_BUTTON_PLAY )
+;\2 key ( rawkey ) ( for example KEY_LEFT_ALT or $64 )
+
+	;button or direct was pressed ?
+		btst	#JPB_\1,d1
+		beq.w	.free_key\@
+
+	;if last time the same was pressed then jump to next key
+		btst	#JPB_\1,d2
+		bne.w	.next_key\@
+
+	;set appropriate bit in prev / last cd32pad bit state
+		bset	#JPB_\1,d2
+
+	;emulate key ( press )
+		move.b	#\2,d0
+
+	;call routine
+		movem.l	d0/a0/a1,-(sp)
+		move.l	gamePortsRoutine(pc),a0
+		jsr	(a0)
+		movem.l	(sp)+,d0/a0/a1
+		bra.w	.next_key\@
+
+.free_key\@
+
+	;is this real reason to emulate ( unpressed key )
+		btst	#JPB_\1,d2
+		beq.w	.next_key\@
+
+	;clean proper bit in prev / last cd32pad bit state
+		bclr	#JPB_\1,d2
+
+	;emulate key ( unpress )
+		move.b	#\2-$1,d0
+
+	;call routine
+		movem.l	d0/a0/a1,-(sp)
+		move.l	gamePortsRoutine(pc),a0
+		jsr	(a0)
+		movem.l	(sp)+,d0/a0/a1
+
+.next_key\@
+	ENDM
+;-----------------------------------------------------------------------------
+;
+; W H D L O A D   L A B E L S
+;  
+
 CHIPMEMSIZE	= $80000
 FASTMEMSIZE	= $80000
-	ENDC
-
 NUMDRIVES	= 1
 WPDRIVES	= %0000
 
-;DISKSONBOOT
-DOSASSIGN
-;INITAGA
-HDINIT
-IOCACHE		= 10000
-;MEMFREE	= $200
-;NEEDFPU
-;SETPATCH
-;STACKSIZE = 10000
-BOOTDOS
 CACHE
+HDINIT
+IOCACHE		= 1024
+BOOTDOS
 
-slv_Version	= 16
+	IFD RELEASE
+
+BLACKSCREEN
+
+	ELSE
+
+DEBUG
+HRTMON
+
+	ENDC	;IFD RELEASE 
+;-----------------------------------------------------------------------------
+slv_Version	= 17
 slv_Flags	= WHDLF_NoError|WHDLF_Examine
-slv_keyexit	= $5D	; num '*'
-
-	include	whdload/kick13.s
-
-;============================================================================
-
+slv_keyexit	= $59	;F10
+;-----------------------------------------------------------------------------
+	INCLUDE	whdload/kick13.s
+;-----------------------------------------------------------------------------
 	IFD BARFLY
+	IFND	.passchk
 	DOSCMD	"WDate  >T:date"
+.passchk
+	ENDC
 	ENDC
 
-
-DECL_VERSION:MACRO
-	dc.b	"1.1"
+slv_CurrentDir	dc.b	"data",0
+slv_name	dc.b	"Ugh",0
+slv_copy	dc.b	"1992 PlayByte - Ego Software",0
+slv_info	dc.b	"installed & fixed by asman & JOTD",10
+		dc.b	"Version 1.2 "
 	IFD BARFLY
-		dc.b	" "
 		INCBIN	"T:date"
 	ENDC
-	ENDM
-
-slv_name		dc.b	"Ugh"
-	IFD	CHIP_ONLY
-	dc.b	" (DEBUG MODE)"
-	ENDC
+			dc.b	10
+			dc.b	"Thanks to Christian Sauer for original",10,10
+			dc.b	"=============< BUTTONS >===============",10
+			dc.b	"blue   = F1  key (start game)",10
+			dc.b	"green  = ESC key (back to title menu)",10
+			dc.b	"yellow = F3  key (easy, medium, hard)",10
+			dc.b	"play   = P   key (pause in game)",10,10
+			dc.b	"Trainer added by Arise from Decay",10
 			dc.b	0
-slv_copy		dc.b	"1992 PlayByte - Ego Software",0
-slv_info		dc.b	"adapted by Asman & JOTD",10,10
-		dc.b	"Version "
-		DECL_VERSION
-		dc.b	0
-slv_CurrentDir:
-	dc.b	"data",0
+slv_config	dc.b	"C1:X:Unlimited Lives:0;"
+			dc.b	"C1:X:Unlimited Energy:1;"
+			dc.b    "C2:X:Soft Landing:0;"
+			dc.b	"C2:X:Invulnerability:1;"
+			dc.b	"C3:X:Always max Bonus:0;"
+			dc.b	"C3:X:Start with 50 Lives:1;"
+			dc.b	"C5:B:Show cracktro;"
+			dc.b	0
 
-intro:
-	dc.b	"loader",0
-program:
-	dc.b	"t",0
-highs:
-	dc.b	"ugh.high",0
-args		dc.b	10
-args_end
-	dc.b	0
+    EVEN
 
-; version xx.slave works
 
-	dc.b	"$","VER: slave "
-	DECL_VERSION
-	dc.b	0
+;-----------------------------------------------------------------------------
+_bootdos:
+		move.l _resload(pc),a2	   ;A2 = resload
+		bsr	_detect_controller_types
+ 
+	;open doslib
+		lea	_dosname(pc),a1
+		move.l	(4),a6
+		jsr	_LVOOldOpenLibrary(a6)
+		lea	_dosbase(pc),a0
+		move.l	d0,(a0)
+		move.l	d0,a6			;A6 = dosbase
+	;load cracktro
+		clr.l	-(a7)
+		clr.l	-(a7)
+		pea	WHDLTAG_CUSTOM5_GET
+		move.l	a7,a0
+		jsr	(resload_Control,a2)
+		tst.l	(4,a7)
+		beq	.noctro
+		lea cracktro(pc),a3
+		move.l  a3,d1
+		jsr _LVOLoadSeg(a6)
+		move.l  d0,d7
+		beq error
+
+	;run cracktro
+	    lsl.l   #2,d0
+	    move.l  d0,a3
+	    jsr 4(a3)
+	;remove cracktro
+		move.l	d7,d1
+		move.l	_dosbase(pc),a6
+		jsr	_LVOUnLoadSeg(a6)
+.noctro
+	;load intro
+		lea	intro(pc),a3
+		move.l	a3,d1
+		jsr	_LVOLoadSeg(a6)
+		move.l	d0,d7			;D7 = seglist
+		beq	error
+	;patch intro
+
+	;run intro
+		lsl.l	#2,d0
+		move.l	d0,a3
+		jsr	4(a3)
+
+	;remove intro
+		move.l	d7,d1
+		move.l	_dosbase(pc),a6
+		jsr	_LVOUnLoadSeg(a6)
+
+	;load game
+		lea	game(pc),a3
+		move.l	a3,d1
+		jsr	_LVOLoadSeg(a6)
+		move.l	d0,d7
+		beq	error
+
+	;patch game
+		lea	patch_hunk_0(pc),a0
+		move.l	d7,a1
+		move.l	_resload(pc),a2
+		jsr	resload_PatchSeg(a2)
+
+PORTS_OFFSET	= $51a4
+		move.l	d7,a1
+		add.l	a1,a1
+		add.l	a1,a1
+		add.l	#PORTS_OFFSET+4,a1
+		lea	gamePortsRoutine(pc),a3
+		move.l	a1,(a3)
+
+	; get Hiscore address ( hunk 7 + hiscore_offset )
+HISCORE_HUNK	= 7
+HISCORE_OFFSET	= $1fe
+HISCORE_SIZE	= 106
+
+		moveq	#HISCORE_HUNK,d0
+		move.l	d7,a1
+		bsr	find_hunk
+		add.l	a1,a1
+		add.l	a1,a1
+		add.l	#HISCORE_OFFSET+4,a1
+		lea	(pHiscore,pc),a0
+		move.l	a1,(a0)
+
+	; load highscore
+
+		bsr	load_hiscore
+
+	;run game
+		lsl.l	#2,d7
+		move.l	d7,a3
+		jsr	4(a3)
+
+_exit		pea	(TDREASON_OK).w
+_quit		move.l	(_resload,pc),-(a7)
+		addq.l	#resload_Abort,(a7)
+		rts
+
+_debug		pea	(TDREASON_DEBUG).w
+		bra	_quit 
+
+error		jsr	_LVOIoErr(a6)
+		move.l	a3,-(sp)
+		move.l	d0,-(sp)
+		pea	TDREASON_DOSREAD
+		bra	_quit
+
+;-----------------------------------------------------------------------------
+patch_hunk_0
+		PL_START
+		PL_IFC1X 0
+        PL_NOPS	$5698,3		;lives
+        PL_ENDIF
+		PL_IFC1X 1
+		PL_NOPS	$53de,3
+		;PL_NOPS $53e2,1
+		PL_NOPS $5424,3
+		;PL_NOPS $5428,1	 ;energy
+		PL_NOPS	$548a,3
+		;PL_NOPS $548e,1
+		PL_NOPS	$54dc,3
+		;PL_NOPS $54e0,1
+		PL_ENDIF
+		PL_IFC2X 0
+		PL_W	$5582,$600c   	;landing check
+	    PL_ENDIF
+		PL_IFC2X 1
+		PL_W	$5582,$600c
+		PL_NOPS	$761c,2
+		PL_NOPS	$765c,2
+		PL_NOPS	$76c2,2
+		PL_NOPS	$776c,2
+		PL_W	$66e4,$0
+		PL_ENDIF
+		PL_IFC3X 0
+		PL_NOPS	$7cf2,2
+		PL_ENDIF
+		PL_IFC3X 1
+		PL_W	$7248,50
+		PL_ENDIF
+    ; fix access fault (movec vbr,a1 --> sub.l a1,a1 + nop)
+		PL_L	$18,$93c94e71
+
+		PL_P	$516a,setupKeyboard
+
+	; change int PORTS to routine
+		PL_NOP	$51b0,$10
+		PL_R	$5224
+
+	; CD32 support
+		PL_PS	$399a,fixCD32_int		
+
+	; save hiscore
+		PL_PS	$2506,fixHiscore_Save
+		PL_END
+
+;-----------------------------------------------------------------------------
+fixCD32_int
+		move.w	#$3e8,d0	;original
+		nop			;original
+
+		movem.l	d0-d2/a1,-(sp)
+
+		bsr	_read_joysticks_buttons
+		move.l	joy1_buttons(pc),d0 
+		move.l	d0,d1
+
+		btst	#JPB_BTN_REVERSE,d0
+		beq.b	.state
+		btst	#JPB_BTN_FORWARD,d0
+		beq.b	.state
+
+		bra	_exit
+
+.state		lea	(cd32last_state,pc),a1
+		move.l	(a1),d2
+
+		Emulate_Key	BUTTON_BLUE,$5f		;F1
+		Emulate_Key	BUTTON_GREEN,$75	;ESC
+		Emulate_Key	BUTTON_PLAY,$CD		;P
+		Emulate_Key	BUTTON_YELLOW,$5B	;F3
+
+		move.l	d2,(a1)
+		movem.l	(sp)+,d0-d2/a1
+		rts
+
+cd32last_state	dc.l	0
+
+;-----------------------------------------------------------------------------
+
+setupKeyboard
+	;set the interrupt vector
+		pea	(.int,pc)
+		move.l	(a7)+,($68)
+	;allow interrupts from the keyboard
+		move.b	#CIAICRF_SETCLR|CIAICRF_SP,(ciaicr+_ciaa)
+	;clear all ciaa-interrupts
+		tst.b	(ciaicr+_ciaa)
+	;set input mode
+		and.b	#~(CIACRAF_SPMODE),(ciacra+_ciaa)
+	;clear ports interrupt
+		move.w	#INTF_PORTS,(intreq+_custom)
+	;allow ports interrupt
+		move.w	#INTF_SETCLR|INTF_INTEN|INTF_PORTS,(intena+_custom)
+		rts 
+
+	;check if keyboard has caused interrupt
+.int		btst	#INTB_PORTS,(intreqr+1+_custom)
+		beq	.end
+		btst	#CIAICRB_SP,(ciaicr+_ciaa)
+		beq	.end
+
+		bsr	_KeyboardHandle
+
+.end		move.w	#INTF_PORTS,(intreq+_custom)
+	;to avoid timing problems on very fast machines we do another
+	;custom access
+		tst.w	(intreqr+_custom)
+		rte 
+
+_KeyboardHandle	movem.l	d0-d1/a0-a1,-(a7)
+		lea	(_custom),a0
+		lea	(_ciaa),a1
+	;read keycode
+		moveq	#0,d0
+		move.b	(ciasdr,a1),d0
+	;set output mode (handshake)
+		or.b	#CIACRAF_SPMODE,(ciacra,a1)
+
+		movem.l	d0/a0/a1,-(sp)
+		move.l	gamePortsRoutine(pc),a0
+		jsr	(a0)
+		movem.l	(sp)+,d0/a0/a1
+
+	;calculate rawkeycode
+		not.b	d0
+		ror.b	#1,d0
+
+		cmp.b	(_keydebug,pc),d0
+		bne	.1
+		movem.l	(a7)+,d0-d1/a0-a1		;restore
+	;transform stackframe to resload_Abort arguments
+		move.w	(a7),(6,a7)			;sr
+		move.l	(2,a7),(a7)			;pc
+		clr.w	(4,a7)				;ext.l sr
+		bra	_debug
+
+.1		cmp.b	(_keyexit,pc),d0
+		beq	_exit
+	
+	;better would be to use the cia-timer to wait, but we aren't know if
+	;they are otherwise used, so using the rasterbeam
+	;required minimum waiting is 85 µs, one rasterline is 63.5 µs
+	;a loop of 3 results in min=127µs max=190.5µs
+
+		moveq   #3-1,d1
+.wait1		move.b	(vhposr,a0),d0
+.wait2		cmp.b	(vhposr,a0),d0
+		beq	.wait2
+		dbf	d1,.wait1
+
+	;set input mode
+		and.b	#~(CIACRAF_SPMODE),(ciacra,a1)
+		movem.l	(a7)+,d0-d1/a0-a1
+		rts
+
+;-----------------------------------------------------------------------------
+load_hiscore
+		movem.l	d0-a6,-(sp)
+
+		lea	(file_hiscore,pc),a0
+		WHDL	GetFileSize
+		tst.l	d0
+		beq	.end
+
+		lea	(file_hiscore,pc),a0
+		move.l	(pHiscore,pc),a1
+		WHDL	LoadFile
+	
+		bsr	cryptrout	; <-- crypt routine
+	
+.end		movem.l	(sp)+,d0-a6
+		rts
+
+
+;-----------------------------------------------------------------------------;
+fixHiscore_Save
+
+		move.b	#13,(8,a2)	; orginal
+
+		movem.l	d0-a6,-(sp)
+
+		bsr	cryptrout	; <-- crypt routine for write 
+
+		moveq	#HISCORE_SIZE,d0
+		move.l	(pHiscore,pc),a1
+		
+		lea	(file_hiscore,pc),a0
+		WHDL	SaveFile
+
+		bsr	cryptrout	; <-- decrypt routine
+
+		movem.l	(sp)+,d0-a6
+
+		rts
+
+;-----------------------------------------------------------------------------
+
+cryptrout	movem.l	d0/a0,-(sp)
+
+		move.l	(pHiscore,pc),a0
+
+		moveq	#HISCORE_SIZE/4-1,d0
+
+.loop		eor.l	#$9bedafec,(a0)+
+		dbf	d0,.loop
+		
+		movem.l	(sp)+,d0/a0
+		rts
+
+;-----------------------------------------------------------------------------
+find_hunk
+		tst.l	d0	;test for hunk 0
+		beq	.exit	;yes that's hunk is 0, so now we have 
+				;in a1 - BCPL pointer to this hunk
+		subq.l	#1,d0
+
+.loop		adda.l	a1,a1
+		adda.l	a1,a1	;BCPL * 4 = APTR of hunk
+		move.l	(a1),a1	;get BCPL pointer to next hunk
+
+
+	IFD DEBUG_MODE
+		movem.l	d1,-(sp)
+
+		move.l	a1,d1
+		tst.l	d1
+		bne	.ok
+
+		movem.l	(sp)+,d1
+
+		pea	(.msg_bad_hunk,pc)
+		pea	TDREASON_FAILMSG
+		bra	_exit
+		
+.msg_bad_hunk	dc.b	"Bad hunk number",0
 	EVEN
 
-_bootdos
-		clr.l	$0.W
+.ok		movem.l	(sp)+,d1
 
-	; saves registers (needed for BCPL stuff, global vector, ...)
+	ENDC	;IFD DEBUG_MODE
 
-		lea	(_saveregs,pc),a0
-		movem.l	d1-d7/a1-a2/a4-a6,(a0)
-		lea	_stacksize(pc),a2
-		move.l	4(a7),(a2)
+		dbf	d0,.loop
 
-		move.l	_resload(pc),a2		;A2 = resload
+.exit		rts
 
-	;get tags
-		lea	(tag,pc),a0
-		jsr	(resload_Control,a2)
-	
-	;open doslib
-		lea	(_dosname,pc),a1
-		move.l	(4),a6
-		jsr	(_LVOOldOpenLibrary,a6)
-		move.l	d0,a6			;A6 = dosbase
+;-----------------------------------------------------------------------------
+	INCLUDE	'ReadJoyPad.s' 
+;-----------------------------------------------------------------------------
 
-	;assigns
-;		lea	assign(pc),a0
-;		sub.l	a1,a1
-;		bsr	_dos_assign
-
-	;load exe
-		lea	intro(pc),a0
-		lea	args(pc),a1
-		moveq	#args_end-args,d0
-		sub.l	a5,a5
-		bsr	load_exe
-	;quit
-	;load exe
-		lea	program(pc),a0
-		lea	args(pc),a1
-		moveq	#args_end-args,d0
-		lea	patch_main(pc),a5
-		bsr	load_exe
-	;quit
-_quit		pea	TDREASON_OK
-		move.l	(_resload,pc),a2
-		jmp	(resload_Abort,a2)
-
-load_highs:
-	MOVEM.L	D0-D7/A0-A6,-(A7)	;1d2: 48e7fffe
-	LEA	highs(PC),A0	;1d6: 41fafed1
-	MOVEA.L	_resload(PC),A2	;1da: 247a0d92
-	JSR	resload_GetFileSize(A2)			;1de: 4eaa0024
-	TST.L	D0			;1e2: 4a80
-	BEQ.W	LAB_000B		;1e4: 67000012
-	LEA	highs(PC),A0	;1e8: 41fafebf
-	MOVEA.L	hiscore_buffer(PC),A1		;1ec: 227a0054
-	JSR	resload_LoadFile(A2)			;1f0: 4eaa0008
-	BSR.W	hiscore_encrypt		;1f4: 61000032
-LAB_000B:
-	MOVEM.L	(A7)+,D0-D7/A0-A6	;1f8: 4cdf7fff
-	RTS				;1fc: 4e75
-
-save_highs:
-	MOVE.B	#$0d,8(A2)		;1fe: 157c000d0008 original code
-	MOVEM.L	D0-D7/A0-A6,-(A7)	;204: 48e7fffe
-	BSR.W	hiscore_encrypt		;208: 6100001e
-	MOVEQ	#106,D0			;20c: 706a
-	MOVEA.L	hiscore_buffer(PC),A1		;20e: 227a0032
-	LEA	highs(PC),A0	;212: 41fafe95
-	MOVEA.L	_resload(PC),A2	;216: 247a0d56
-	JSR	resload_SaveFile(A2)			;21a: 4eaa000c
-	BSR.W	hiscore_encrypt		;21e: 61000008
-	MOVEM.L	(A7)+,D0-D7/A0-A6	;222: 4cdf7fff
-	RTS				;226: 4e75
-
-hiscore_encrypt:
-	MOVEM.L	D0/A0,-(A7)		;228: 48e78080
-	MOVEA.L	hiscore_buffer(PC),A0		;22c: 207a0014
-	MOVEQ	#25,D0			;230: 7019
-LAB_000E:
-	EORI.L	#$9bedafec,(A0)+	;232: 0a989bedafec
-	DBF	D0,LAB_000E		;238: 51c8fff8
-	MOVEM.L	(A7)+,D0/A0		;23c: 4cdf0101
-	RTS				;240: 4e75
-
-hiscore_buffer:
-	dc.l	0			;242: 00000000
-	
-; < d7: seglist (APTR)
-
-patch_main
-	moveq.l	#8,d2
-	bsr	get_section
-	ADDA.L	#$1fe,A1		;11e: d1fc000001fe
-	LEA	hiscore_buffer(PC),A2		;124: 45fa011c
-	MOVE.L	A1,(A2)			;128: 2488
-
-	moveq.l	#0,d2
-	bsr	get_section
-
-	lea	pl_main(pc),a0
-	move.l	_resload(pc),a2
-	jsr	resload_Patch(a2)
-	
-	BSR.W	load_highs		;188: 61000048
-	rts
-	
-pl_main
-	PL_START
-	PL_L	$18,$93c94e71	; remove VBR access	(sub.l  A1,A1 + NOP)
-	; keyboard
-	PL_PSS	$51B2,kbdelay,8
-
-
-	PL_NOP	$5224,16
-	PL_PS	$2506,save_highs
-	PL_END
-	
-CIAB_PRA	EQU	$BFD000
-CIAA_SDR	EQU	$BFEC01
-CIAA_CRA	EQU	$BFEE01
-VHPOSR		EQU	$DFF006
-
-kbdelay:
-	MOVEM.L	D1-D2,-(A7)		;194: 48e76000
-	MOVE.B	CIAA_SDR,D0		;198: 103900bfec01
-	MOVE.B	#$00,CIAA_SDR		;19e: 13fc000000bfec01
-	ORI.B	#$40,CIAA_CRA		;1a6: 0039004000bfee01
-	MOVEQ	#2,D1			;1ae: 7202
-LAB_0008:
-	MOVE.B	VHPOSR,D2		;1b0: 143900dff006
-LAB_0009:
-	CMP.B	VHPOSR,D2		;1b6: b43900dff006
-	BEQ.W	LAB_0009		;1bc: 6700fff8
-	DBF	D1,LAB_0008		;1c0: 51c9ffee
-	ANDI.B	#$bf,CIAA_CRA		;1c4: 023900bf00bfee01
-	move.b	d0,d1
-	ror.b	#1,d1
-	not.b	d1
-	cmp.b	_keyexit(pc),d1
-	bne.b	.out
-	pea	TDREASON_OK
-	move.l	_resload(pc),-(a7)
-	addq.l	#resload_Abort,(a7)
-	rts
-.out
-	MOVEM.L	(A7)+,D1-D2		;1cc: 4cdf0006
-	RTS
-	
-; < d7 seglist
-; < d2 section #
-; > a1 segment
-get_section
-	move.l	d7,a1
-	subq	#1,d2
-	bmi.b	.out
-.loop
-	move.l	(a1),a1
-	add.l	a1,a1
-	add.l	a1,a1
-	dbf	d2,.loop
-.out
-	addq.l	#4,a1
-	rts
-
-
-
-
-; < a0: program name
-; < a1: arguments
-; < d0: argument string length
-; < a5: patch routine (0 if no patch routine)
-
-
-load_exe:
-	movem.l	d0-a6,-(a7)
-	move.l	d0,d2
-	move.l	a0,a3
-	move.l	a1,a4
-	move.l	a0,d1
-	jsr	(_LVOLoadSeg,a6)
-
-	move.l	d0,d7			;D7 = segment
-	beq	.end			;file not found
-
-	;patch here
-	cmp.l	#0,A5
-	beq.b	.skip
-	movem.l	d2/d7/a4,-(a7)
-	add.l	d7,d7
-	add.l	d7,d7
-	jsr	(a5)
-	bsr	_flushcache
-	movem.l	(a7)+,d2/d7/a4
-.skip
-	;call
-	move.l	d7,a3
-	add.l	a3,a3
-	add.l	a3,a3
-
-	move.l	a4,a0
-
-	movem.l	d7/a6,-(a7)
-
-	move.l	d2,d0			; argument string length
-	move.l	_stacksize(pc),-(a7)	; original stack format
-	movem.l	(_saveregs,pc),d1-d7/a1-a2/a4-a6	; original registers (BCPL stuff)
-	jsr	(4,a3)		; call program
-	addq.l	#4,a7
-
-	movem.l	(a7)+,d7/a6
-
-	;remove exe
-
-	move.l	d7,d1
-	jsr	(_LVOUnLoadSeg,a6)
-
-	movem.l	(a7)+,d0-a6
-	rts
-
-.end
-	jsr	(_LVOIoErr,a6)
-	move.l	a3,-(a7)
-	move.l	d0,-(a7)
-	pea	TDREASON_DOSREAD
-	move.l	(_resload,pc),-(a7)
-	add.l	#resload_Abort,(a7)
-	rts
-
-_saveregs
-		ds.l	16,0
-_stacksize
-		dc.l	0
-
-tag		dc.l	WHDLTAG_CUSTOM1_GET
-custom1	dc.l	0
-		dc.l	0
-
-;============================================================================
-
-	END
+pHiscore	dc.l	0	;highscore adress
+_dosbase:	dc.l	0
+introSegList:	dc.l	0
+gamePortsRoutine:	dc.l	0	;address of game keyboard decode routine
+file_hiscore	dc.b	"Ugh.highs",0
+	EVEN
+intro:		dc.b	'loader',0
+game:		dc.b	't',0
+cracktro	dc.b	"Decade",0
